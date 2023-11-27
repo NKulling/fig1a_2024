a8f9af96ac8fa4fa91d1664874b9b70d2edbee90
NKulling <78426848+NKulling@users.noreply.github.com>
2023-11-27 15:39:54 +0100

    Add files via upload

---
+++ spat_point.R
@@ -0,0 +1,151 @@
+
+library(pxR)
+library(dplyr)
+library(terra)
+library(data.table)
+
+# Loading data ------------------------------------------------------------
+
+setwd("C:/Users/kuelling/Documents/Conferences/Fig1a/2023/code")
+file<-"C:/Users/kuelling/Documents/Conferences/Fig1a/2023/cheptel/px-x-0702000000_104.px"
+mun<-vect("C:/Users/kuelling/Documents/VALPAR/DATA/Swiss boundaries/swissboundaries3d_2023-01_2056_5728.shp/swissBOUNDARIES3D_1_4_TLM_HOHEITSGEBIET.shp")
+
+wolf<-rast("C:/Users/kuelling/Documents/Conferences/Fig1a/2023/canis_lupus/data/Canis.lupus_reg_covariate_ensemble.tif")
+evals<-fread("C:/Users/kuelling/Documents/VALPAR/ES Assessment/SDM/EVALS_and_COV/evals_reg_covariate.csv")
+
+cntons<-vect("C:/Users/kuelling/Documents/VALPAR/DATA/Swiss boundaries/swissBOUNDARIES3D_1_3_TLM_KANTONSGEBIET.shp")
+vs<-cntons[cntons$KANTONSNUM==23,]
+
+
+# Processing OFS data -----------------------------------------------------
+
+### OFS data
+
+data = read.px(file, na.strings = c('"."','".."','"..."','"...."','"....."','"....."','":"'))
+table= as.data.frame(data)
+
+##- Selection 
+
+colnames(table)[3] <- "comm"
+
+subset_df <- table%>%
+  filter(str_starts(str_trim(comm), "......"),
+         Jahr=="2020",                                 # Year
+         Beobachtungseinheit %in% c("Tiere - Rinder", #  Beef
+                                    "Tiere - Ziege",  # goat
+                                    "Tiere - Schafe"), # sheep
+         Betriebssystem == "Betriebssystem - Total",  # Bio and conventional
+         !str_detect(comm, "^>>"), #keeping municipalities only
+         !str_detect(comm, "^-"),
+         !str_detect(comm, "^Schweiz")
+         )
+
+# reworking municipality string
+
+ndf<- subset_df%>%
+  mutate(mun_id = as.numeric(str_extract(comm, "\\d+")))
+
+#  Summing all bestioles
+
+ndf2 <- ndf %>%
+  group_by(mun_id) %>%
+  summarize(total_chept = sum(value, na.rm = TRUE))
+
+
+# Combining OFS and spatial municipality data -----------------------------
+
+t<-merge(mun,ndf2,by.x="BFS_NUMMER", by.y= "mun_id")
+
+writeVector(t,"C:/Users/kuelling/Documents/Conferences/Fig1a/2023/mun/mun_cheptel.shp" )
+
+plot(t, col=t$total_chept, type="continuous")
+
+
+
+# Generate random points --------------------------------------------------
+# in each municipality extent, based on the amount of bestioles present. 
+
+pointzz<-spatSample(t[1],t[1]$total_chept)
+
+for(i in 2:nrow(t)){
+  
+  nb_pt<-t[i]$total_chept
+  
+  if(nb_pt <1){next}
+  else{
+  poinz<-spatSample(t[i],nb_pt)
+  
+  pointzz<-rbind(poinz,pointzz)
+  
+  print(t[i]$NAME)
+  }
+  gc()
+}
+
+writeVector(pointzz, "C:/Users/kuelling/Documents/Conferences/Fig1a/2023/mun/points_cheptel.shp" )
+
+print("done")
+
+
+# Processing wolf map -----------------------------------------------------
+
+thr<-(evals[evals$species == "Canis lupus",]$Threshold+0.2) * 100
+
+m<-c(-Inf,thr,NA,
+     thr,Inf,1)
+
+m1<-matrix(m, byrow=T, ncol=3)
+
+wolf_bi<-classify(wolf, m1)
+
+
+# generating wolf points over the range -----------------------------------
+
+wolfz<-300 #rough and very conservative estimation
+
+wolfs<-spatSample(wolf_bi,wolfz,xy=T, na.rm=T)
+
+wolfs_points<-vect(wolfs[,c(1,2)], geom= c("x","y"), crs="epsg:2056")
+
+writeVector(wolfs_points, "C:/Users/kuelling/Documents/Conferences/Fig1a/2023/mun/points_wolfs.shp", overwrite=T )
+
+
+# generating wolf pack points ---------------------------------------------
+
+packs<-32 #https://www.swissinfo.ch/eng/sci-tech/new-wolf-pack-identified-in-switzerland/48301408
+
+set.seed(12)
+packz<-spatSample(wolf_bi,packs,xy=T,na.rm=T, method="stratified")
+
+packs_points<-vect(packz[,c(1,2)], geom= c("x","y"), crs="epsg:2056")
+
+packs_buff<-buffer(packs_points, 100)
+
+a<-spatSample(packs_buff[1], wolfz/nrow(packs_buff), method= "random")
+
+for(i in 2:nrow(packs_buff)){
+  
+  b<-spatSample(packs_buff[i], wolfz/nrow(packs_buff), method= "random")
+  
+  a<-rbind(a,b)
+  
+}
+
+writeVector(a, "C:/Users/kuelling/Documents/Conferences/Fig1a/2023/mun/wolfpacks.shp", overwrite=T)
+
+
+# Crop to Valais extent ---------------------------------------------------
+
+a_vs<-mask(a,vs)
+
+points<-vect("C:/Users/kuelling/Documents/Conferences/Fig1a/2023/mun/points_cheptel.shp")
+
+pt_vs<-mask(points,vs)
+
+
+
+writeVector(a_vs, "C:/Users/kuelling/Documents/Conferences/Fig1a/2023/mun/wolfpacks_vs.shp", overwrite=T)
+
+writeVector(pt_vs, "C:/Users/kuelling/Documents/Conferences/Fig1a/2023/mun/cheptel_vs.shp", overwrite=T)
+
+
